26e57fd43828e601263bf0bc57d3a1af0c8af3b3
kalyani2333 <91599782+kalyani2333@users.noreply.github.com>
2022-01-07 17:36:46 +0530

    Initial Commit

---
+++ loginScreen/ProfilePageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePageViewController.swift
+//  loginScreen
+//
+//  Created by Admin on 07/01/22.
+//
+
+import UIKit
+
+class ProfilePageViewController: UIViewController {
+
+    @IBOutlet weak var imageView1: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView1.layer.borderColor = UIColor.white.cgColor
+        imageView1.layer.borderWidth = 5
+    }
+}
